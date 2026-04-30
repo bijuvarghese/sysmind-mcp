b4a9b42dc0a53f1dfe34c5bf3ed5475acaa423f2
Biju Varghese <bijukavanal@gmail.com>
2026-04-30 16:28:50 -0500

    use lombok for appconfig

--- src/main/java/com/bxv/sysmindmcp/config/AppConfig.java
+++ src/main/java/com/bxv/sysmindmcp/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.bxv.sysmindmcp.config;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -11,26 +13,12 @@ import java.time.Duration;
 @Component
 @Validated
 @ConfigurationProperties(prefix = "llm")
+@Getter
+@Setter
 public class AppConfig {
     @NotBlank
     private String url;
 
     @NotNull
     private Duration timeout;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Duration getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(Duration timeout) {
-        this.timeout = timeout;
-    }
 }
